145df5c2bff27e080e462da8335a0ffd787fbf1f
์Nitipoom <Nitipoom.pure2544@gmail.com>
2022-10-17 11:10:58 +0700

    Fixed

---
+++ Action RPG Game Development/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        UnityEngine.Debug.Log("Play Game!");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEngine.Debug.Log("Quit Game!");
+    }
+}
